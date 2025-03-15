1a6dd17d2f22207ef7b67d0b9171b9b6b871469a
Ben Davis <bendavis493@gmail.com>
2025-03-15 09:37:00 -0600

    implemented styled nav bar

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,19 @@
+<script>
+  import {  Link } from "svelte-routing";
+  let links = [['/','Home'],['/login','Login'],['/dashboard','Dashboard'],['/units/1','Units']]
+</script>
+
+<nav class="bg-stone-200 p-2 flex justify-between">
+  <a href="/">
+    <img src="https://placehold.co/50x50" alt="Logo">
+  </a>
+  <div class="flex gap-0.5">
+    {#each links as link}
+      <Link class="pt-1 pb-1 pe-3 ps-3 hover:bg-stone-300 content-center rounded-sm hover:shadow-sm active:shadow-none active:inset-shadow-sm inset-shadow-stone-400" to={link[0]}>{link[1]}</Link>
+    {/each}
+  </div>
+</nav>
+
+
+<style>
+</style>
