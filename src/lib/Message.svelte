e8f8ee2906915cb663df5054dee1e9fea41e2578
Ashish Jain <ashishjain99@yahoo.com>
2025-04-03 14:03:10 -0600

    Dashboard and message err fixed

--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -5,7 +5,7 @@
   </script>
   <div class="flex justify-center content-center full-page flex-wrap">
     <div class="flex h-1/5 bg-stone-100 justify-center rounded-sm shadow-sm flex-col p-40">
-        <h2 class="text-center text-3xl">Request successfully submitted. We will as soon as possible</h2>
+        <h2 class="text-center text-3xl">Request successfully submitted. We will get back to you as soon as possible.</h2>
       
     </div>
 </div>
