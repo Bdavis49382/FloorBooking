4b98237ae96d8da76aeba758d323400ae3a8db81
Rubinel Youan Bi <104086734+Youan22@users.noreply.github.com>
2025-04-03 13:56:45 -0600

    Footer fixed

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -146,11 +146,3 @@
 </div>
 
 
-
-<footer class="bg-gray-800">
-  <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:py-6 lg:px-8">
-    <p class="text-center text-base text-gray-400">
-      &copy; 2025. All Rights Reserved. FloorBooking is a product of FloorBooking Inc.
-    </p>
-  </div>
-</footer>
